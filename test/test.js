02173ff1aec4a9b221c3cb2d8086eb64d42836d8
Maksaud <49394528+Maksaud@users.noreply.github.com>
2020-11-20 14:33:22 +0000

    added a dockerfile

---
+++ test/test.js
@@ -0,0 +1,13 @@
+// async test for http 200 resoponse code using supertest
+'use strict';
+
+var request = requrires("sepertest"),
+    app = require("../app").getApp;
+
+describe('GET /', function(){
+    it('expects HTTP response 200', function(done){
+        request(app)
+        .get('/')
+        .expect(200, done);
+    });
+});
